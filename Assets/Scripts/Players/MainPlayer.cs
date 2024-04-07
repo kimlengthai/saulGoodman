cdab56f5f4b1e7f2bcab25341a94df4ad958e91c
kimlengthai <141606539+kimlengthai@users.noreply.github.com>
2024-04-07 20:22:53 +1000

    Update player's movement

--- Assets/Scripts/Players/MainPlayer.cs
+++ Assets/Scripts/Players/MainPlayer.cs
@@ -26,6 +26,23 @@ public class MainPlayer : Player
         base.Update();
 
         UpdateVisibilityLine();
+
+        if (Input.GetKeyDown(KeyCode.UpArrow)) 
+        {
+            OnUp();
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            OnDown();
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            OnLeft();
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            OnRight();
+        }
     }
 
 
