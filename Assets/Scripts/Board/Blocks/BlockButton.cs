b107b9e992541a4af0d96d6cbfb27fa1a6bc6475
Aymeric Conti <aymeric.conti2002@gmail.com>
2024-04-19 14:34:49 +1000

    fix buttons on back one turn

--- Assets/Scripts/Board/Blocks/BlockButton.cs
+++ Assets/Scripts/Board/Blocks/BlockButton.cs
@@ -7,11 +7,40 @@ public class BlockButton : Block
     [SerializeField] List<BlockDoor> doorsToUnlock;
     [SerializeField] float animationSpeed;
 
+    [SerializeField] bool startingPressed = false;
+
+    bool _pressed;
+    bool pressed
+    {
+        get { return _pressed; }
+        set
+        {
+            _pressed = value;
+
+            if (pressed)
+                UnlockDoors();
+            else
+                LockDoors();
+        }
+    }
+
+
+    protected override void Init()
+    {
+        base.Init();
+        pressed = startingPressed;
+    }
+
 
     protected override void OnPlayerInteract(Player player, Vector2Int playerDirection)
     {
-        UnlockDoors();
-        StartCoroutine(ChangeSpriteColor(Color.green, animationSpeed));
+        pressed = true;
+    }
+
+
+    protected override void UpdateSprite()
+    {
+        StartCoroutine(ChangeSpriteColor(pressed ? Color.green : defaultColor, animationSpeed));
     }
 
 
@@ -26,5 +55,29 @@ public class BlockButton : Block
     {
         foreach (BlockDoor door in doorsToUnlock)
             door.open = false;
+    }
+
+
+    public override Dictionary<string, object> GetData()
+    {
+        Dictionary<string, object> data = base.GetData();
+        data["doorsToUnlock"] = doorsToUnlock.ConvertAll(door => door.coords);
+        data["pressed"] = pressed;
+        return data;
+    }
+
+
+    public override void SetData(Dictionary<string, object> data)
+    {
+        base.SetData(data);
+        StartCoroutine(ResetDoorsToUnlockFromData(data));
+        pressed = (bool)data["pressed"];
+    }
+
+
+    IEnumerator ResetDoorsToUnlockFromData(Dictionary<string, object> data)
+    {
+        yield return new WaitForEndOfFrame();
+        doorsToUnlock = ((List<Vector2Int>)data["doorsToUnlock"]).ConvertAll(coords => Game.board.GetBlock(coords) as BlockDoor);
     }
 }
