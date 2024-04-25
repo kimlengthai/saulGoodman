e6b1f1f4e037686ad3c5a521b3940a07e309717f
Aymeric Conti <aymeric.conti2002@gmail.com>
2024-04-25 22:54:53 +1000

    add stars to menu

--- Assets/Scripts/UI/UI.cs
+++ Assets/Scripts/UI/UI.cs
@@ -6,15 +6,23 @@ using TMPro;
 public class UI : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI turnText;
+    [SerializeField] TextMeshProUGUI levelText;
 
 
     public void Start()
     {
         UpdateTurnText();
+        UpdateLevelText();
     }
 
     public void UpdateTurnText()
     {
         turnText.text = "Turn : " + Game.turn;
+    }
+
+
+    public void UpdateLevelText()
+    {
+        levelText.text = "Level : " + Game.board.levelName;
     }
 }
