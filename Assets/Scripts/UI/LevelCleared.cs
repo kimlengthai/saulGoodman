969d1bf82493787b449bdeb804997745e43449bc
Aymeric Conti <aymeric.conti2002@gmail.com>
2024-04-29 17:40:52 +1000

    stars filling animation on level clear

--- Assets/Scripts/UI/LevelCleared.cs
+++ Assets/Scripts/UI/LevelCleared.cs
@@ -1,17 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
+using UnityEngine.UI;
 
 public class LevelCleared : MonoBehaviour
 {
-    [SerializeField] TextMeshProUGUI scoreText;
+    [SerializeField] Animation scoreAnimation;
+    [SerializeField] Image star1;
+    [SerializeField] Image star2;
+    [SerializeField] Image star3;
 
 
     public void Start()
     {
-        int score = PlayerPrefs.GetInt("Score");
-        
-        scoreText.text = "Score : " + score + " Stars!";
+        if (Game.turn > Game.threeStarsTurns)
+        {
+            star3.enabled = false;
+            if (Game.turn > Game.twoStarsTurns)
+            {
+                star2.enabled = false;
+                if (Game.turn > Game.oneStarTurns)
+                    star1.enabled = false;
+            }
+        }
+
+        scoreAnimation.Play();
     }
 }
