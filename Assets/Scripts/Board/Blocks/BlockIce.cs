ad9b303409b3fff07b58ddcd53f7f97a678ac4ed
kimlengthai <141606539+kimlengthai@users.noreply.github.com>
2024-05-05 22:28:21 +1000

    Add sound to the ice block in all lvls

--- Assets/Scripts/Board/Blocks/BlockIce.cs
+++ Assets/Scripts/Board/Blocks/BlockIce.cs
@@ -5,9 +5,15 @@ using System;
 
 public class BlockIce : Block
 {
+    public AudioSource audioIceBlock;
     protected override void OnPlayerEnter(Player player, Vector2Int playerDirection, bool animate)
     {
         base.OnPlayerEnter(player, playerDirection, animate);
+
+        if (audioIceBlock != null)
+        {
+            audioIceBlock.Play();
+        }
 
         if (player.isSliding)
             return;
